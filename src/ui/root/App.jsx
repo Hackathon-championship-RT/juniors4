48c338bedf144de2dff74477f68dc9321a985a86
Karim <kfmust@gmail.com>
2024-11-29 18:28:04 +0300

    Start Screen

--- src/ui/root/App.jsx
+++ src/ui/root/App.jsx
@@ -1,14 +1,29 @@
 import "./App.css";
 import { generatePlacement } from "../../domain/generator";
+import MenuView from "../views/MenuView/MenuView";
 import PlaygroundView from "../views/PlaygroundView/PlaygroundView";
+import { useState } from "react";
 
 export default function App() {
-  const generatedPlayground = generatePlacement(2);
-  console.log(generatedPlayground);
+  const [difficulty, setDifficulty] = useState(1);
+  const [stage, setStage] = useState(0);
+  const [generatedPlayground, setGneratedPlayground] = useState(
+    generatePlacement(2)
+  );
 
   return (
     <>
-      <PlaygroundView generatedPlayground={generatedPlayground} />
+      {stage === 0 ? (
+        <MenuView
+          onDifficultyChange={(value) => setDifficulty(value)}
+          onStart={() => {
+            setGneratedPlayground(generatePlacement(2));
+            setStage(1);
+          }}
+        />
+      ) : (
+        <PlaygroundView generatedPlayground={generatedPlayground} />
+      )}
     </>
   );
 }
