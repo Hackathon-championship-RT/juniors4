852fa2d9e6bdfc6429ab30ff87a7e890d58c7f45
Адиль Ахметов <>
2024-11-29 17:42:15 +0300

    calculating score added

---
+++ src/domain/calculate.js
@@ -0,0 +1,12 @@
+function calculateScore(correctAnswers, wrongAnswers) {
+    const pointsForCorrect = 10;
+    const pointsForWrong = -5;
+
+    let totalScore = (correctAnswers * pointsForCorrect) + (wrongAnswers * pointsForWrong);
+
+    if (correctAnswers > 5) {
+        totalScore += 20;
+    }
+
+    return totalScore;
+}
