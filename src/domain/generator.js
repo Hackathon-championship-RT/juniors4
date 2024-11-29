7cc9b9240e7612e1c9d04f3aaaeaa1de7c4972df
Адиль Ахметов <>
2024-11-29 15:24:14 +0300

    generator added

--- src/domain/generator.js
+++ src/domain/generator.js
@@ -1,10 +1,35 @@
-// Функция для генерации расстановки карточек
-export function generatePlacement(side) {
-  // Замоканые данные при side = 4
-  return [
-    [0, 1, 2, 3],
-    [1, 2, 0, 3],
-    [3, 2, 1, 0],
-    [1, 3, 0, 2],
-  ];
+function shuffle(array) {
+  return array.sort(() => Math.random() - 0.5);
 }
+
+async function fetchIcons() {
+  try {
+    const response = await fetch('/config/icons.json');
+    if (!response.ok) {
+      throw new Error('Ошибка при загрузке JSON файла');
+    }
+    const icons = await response.json();
+    return icons;
+  } catch (error) {
+    console.error('Ошибка:', error);
+    return [];
+  }
+}
+
+async function generatePlacement(difficulty) {
+  const cars_icons = await fetchIcons();
+
+  if (cars_icons.length === 0) {
+    return [];
+  }
+
+  const slicedCars = cars_icons.slice(0, difficulty);
+  const cards = shuffle([...slicedCars, ...slicedCars]);
+
+  return cards;
+}
+
+// Пример использования
+generatePlacement(0, 4).then(cards => {
+  console.log(cards);
+});
